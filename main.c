8be5dbbe8d953f0dc4e0326bf12c278fe750b174
salmon3 <126031696+salmon3@users.noreply.github.com>
2025-09-05 13:06:58 -0500

    Add files via upload

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <iostream>
+
+/* 
+run this program using the console pauser or add your own getch, system("pause") or input loop
+ */
+ 
+ //comentario
+ 
+
+int main(int argc, char** argv) {
+	printf("hola mundo");
+	return 0;
+}
